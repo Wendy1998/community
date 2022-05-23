776916d243b9aaed4b65a2845bacf8d58c45b3b8
wangz <1499007154@qq.com>
2022-05-23 12:09:09 +0800

    2022.5.23 before security

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -2,9 +2,20 @@ package com.nowcoder.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Configuration
 public class CommunityApplication {
+
+	@PostConstruct
+	public void init(){
+		//解决netty启动冲突问题
+		//Netty4Utils.class中的setAvailableProcessors()中
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
---
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -0,0 +1,65 @@
+package com.nowcoder.community.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.ElasticsearchTimeoutException;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class ElasticsearchService {
+
+    @Autowired
+    private DiscussPostRepository discussRepository;
+
+
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    public void saveDiscussPost(DiscussPost post){
+        discussRepository.save(post);
+    }
+
+    public void deleteDiscussPost(int id){
+        discussRepository.deleteById(id);
+    }
+
+    public SearchHits<DiscussPost> searchDiscussPost(String keyword, int current, int limit) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery(keyword, "title", "content"))
+                .withSorts(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSorts(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSorts(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(current, limit))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        return elasticsearchRestTemplate.search(searchQuery, DiscussPost.class);
+    }
+
+}
